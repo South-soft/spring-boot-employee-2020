f89a3d53365cb19e85a9d67614e2f3d0e097859d
Martin <codingzhengsz@163.com>
2020-07-29 20:50:02 +0800

    [Employee][Lester Martin] test: should_get_1_employee_when_get_employee_given_employee_id_1

---
+++ src/test/java/com/thoughtworks/springbootemployee/service/EmployeeServiceTest.java
@@ -0,0 +1,39 @@
+package com.thoughtworks.springbootemployee.service;
+
+import com.thoughtworks.springbootemployee.entity.Company;
+import com.thoughtworks.springbootemployee.entity.Employee;
+import com.thoughtworks.springbootemployee.repository.CompanyRepository;
+import com.thoughtworks.springbootemployee.repository.EmployeeRepository;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class EmployeeServiceTest {
+    @Mock
+    EmployeeRepository employeeRepository;
+    @InjectMocks
+    EmployeeService employeeService;
+
+    @Test
+    public void should_get_1_employee_when_get_employee_given_employee_id_1() {
+        // given
+        int employeeId = 1;
+        Employee employee = new Employee();
+        when(employeeRepository.findById(employeeId)).thenReturn(Optional.of(employee));
+
+        // when
+        Employee getEmployee = employeeService.getEmployee(employeeId);
+
+        // then
+        assertNotNull(getEmployee);
+    }
+
+}
