c6ee9ac1c681ee0682d4a15fa785c159ca655417
linchuhao <370356141@qq.com>
2020-08-02 13:41:44 +0800

    [Employee][Lester Lin] rename the test.

--- src/test/java/com/thoughtworks/springbootemployee/integrationtest/CompanyIntegrationTest.java
+++ src/test/java/com/thoughtworks/springbootemployee/integrationtest/CompanyIntegrationTest.java
@@ -22,7 +22,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-public class CompanyIntegration {
+public class CompanyIntegrationTest {
 
     @Resource
     MockMvc mockMvc;
